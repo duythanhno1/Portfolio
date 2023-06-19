15441c20e5d283379289ab287f4847ed7b13ad55
VuDHA <vudha.work@gmail.com>
2023-06-19 21:15:10 +0700

    Finish - Vũ

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.owl-member').owlCarousel({
+    items: 1,
+    nav: true,
+    dots: true,
+    margin: 100,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    navText: ["<img src='./images/prev-button.png' alt='prev-btn'>", "<img src='./images/next-button.png' alt='next-btn'>"],
+  });
